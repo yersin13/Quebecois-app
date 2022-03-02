3873f6dffc3d02f74f7b4bcc61183e5294b3ff4e
yersin13 <52936287+yersin13@users.noreply.github.com>
2022-03-02 00:18:07 -0500

    some fact added

--- src/data-lessons.ts
+++ src/data-lessons.ts
@@ -33,6 +33,38 @@ export const facts =[
         src: 'https://carnaval.qc.ca/en/',
         img:'./assets/images-main/clip.png'
     },
-    
+    {
+        id: 'facts4',
+        name: 'Le chiac',
+        p1: "Chiac is a variant of French spoken in New Brunswick, mainly in Moncton and in the southeast of the province.",
+        p2: "This mixture of French and English inspires several singers (Radio, Lisa LeBlanc, Les Hay Babies) and writers (Jean Babineau, France Daigle), who choose this language to highlight their identity.",
+        p3: "",
+        ex1:"",
+        ex2:"",
+        src: 'https://carnaval.qc.ca/en/',
+        img:'./assets/images-main/clip.png'
+    },
+    {
+        id: 'facts5',
+        name: 'Les Félix',
+        p1: "\"Les Félix\" are prizes awarded to artists who work in the field of music in Quebec.",
+        p2: "\"Les Félix\", named in honor of the singer-songwriter Félix Leclerc, are distributed every year in the context of the Gala de l'ADISQ.",
+        p3: "",
+        ex1:"l'ADISQ: Association québécoise de l'industrie du disque, du spectacle et de la video ( Québec Association for the Recording, Concert and Video Industries)",
+        ex2:"",
+        src: 'https://carnaval.qc.ca/en/',
+        img:'./assets/images-main/clip.png'
+    },
+    {
+        id: 'facts6',
+        name: 'Les cégeps',
+        p1: "\"Les cégeps\"  is a college of general and professional studies that offers training at the pre-university and technical levels.",
+        p2: "At the end of their cégep studies, which generally last 2 or 3 years depending on the program, students obtain a college diploma (DEC) which allows them either to continue their studies at university or to enter the labor market.",
+        p3: "",
+        ex1:"There are approximately 48 cégeps in Quebec.",
+        ex2:"",
+        src: 'https://carnaval.qc.ca/en/',
+        img:'./assets/images-main/clip.png'
+    },
 
 ]
